08f58793e461062582098d0e99419dffbb940d39
Nadim Anwar <anwarnadim3@gmail.com>
2023-07-12 21:59:47 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(prop) {
+    return (
+        <div className="flex justify-between text-white items-center pt-5">
+            <h3 className={`${prop.dark ? "text-gray" : "text-navy-blue"} font-bold text-sm text-center`}>{new Date().getFullYear()}</h3>
+        </div>
+    )
+}
