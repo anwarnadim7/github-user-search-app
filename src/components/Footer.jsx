8606f687186f6506e54a772282e8d7ac691a6624
Nadim Anwar <anwarnadim3@gmail.com>
2023-07-12 22:23:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(prop) {
     return (
         <div className="flex justify-between text-white items-center pt-5">
-            <h3 className={`${prop.dark ? "text-gray" : "text-navy-blue"} font-bold text-sm text-center`}>Project By - Nadim Anwar | Year {new Date().getFullYear()}</h3>
+            <h3 className={`${prop.dark ? "text-gray" : "text-navy-blue"} font-bold text-sm text-center`}>Project By - Nadim Anwar | Project Year - {new Date().getFullYear()}</h3>
         </div>
     )
 }
